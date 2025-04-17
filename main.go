268d741300c186c474aa074bdf7cec8cc43ab984
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2025-04-17 16:11:01 +0600

    added package level and block level variable using  short and longdeclaration operator

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"github.com/hasanashik/puppy"
 )
 
+var color string = "Red"
+
+const brand string = "Tesla"
+
 func main() {
+	model := "Model 3"
 	fmt.Println("hello world")
 	fmt.Println("Puppy barks: ", puppy.Bark())
 	fmt.Println(puppy.BigBark())
+	fmt.Printf("This is a %v car with color %v and the model is %v", brand, color, model)
 }
