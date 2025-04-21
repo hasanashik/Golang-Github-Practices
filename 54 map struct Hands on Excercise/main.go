eb59d9a6c2b50ef9b75005212d52622398168346
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2025-04-21 23:40:10 +0600

    Hands-on exercise #54 - map struct

---
+++ 54 map struct Hands on Excercise/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	type person struct {
+		fname             string
+		lname             string
+		febIceCreamFlavor []string
+	}
+	var zaman person
+	zaman.fname = "Hasan"
+	zaman.lname = "Ashik"
+	zaman.febIceCreamFlavor = []string{"vanila", "chocolate"}
+	fmt.Printf("%#v", zaman, "\n")
+
+	myMap := make(map[string][]string)
+	myMap[zaman.lname] = []string{"Hasan", "Ashik", "vanila", "chocolate"}
+	for _, v := range myMap {
+		fmt.Println(v)
+	}
+
+}
