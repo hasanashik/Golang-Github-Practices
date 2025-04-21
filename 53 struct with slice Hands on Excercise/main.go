3d81382a01c21bd582ce84b5221bd075e6a62cba
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2025-04-21 23:32:35 +0600

    Hands-on exercise #53 - struct with slice

--- 53 struct with slice Hands on Excercise/main.go
+++ 53 struct with slice Hands on Excercise/main.go
@@ -8,12 +8,12 @@ func main() {
 	type person struct {
 		fname             string
 		lname             string
-		febIceCreamFlavor string
+		febIceCreamFlavor []string
 	}
 	var zaman person
 	zaman.fname = "Hasan"
 	zaman.lname = "Ashik"
-	zaman.febIceCreamFlavor = "vanila"
+	zaman.febIceCreamFlavor = []string{"vanila", "chocolate"}
 	fmt.Printf("%#v", zaman)
 
 }
