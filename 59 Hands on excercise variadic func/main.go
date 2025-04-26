42678e25a7f21e3b5681d1834ac153e5498d637a
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2025-04-26 20:54:18 +0600

    Hands-on exercise #59 - variadic func

--- 59 Hands on excercise variadic func/main.go
+++ 59 Hands on excercise variadic func/main.go
@@ -8,6 +8,8 @@ func main() {
 	mySlice := []int{1, 2, 3, 4, 5}
 
 	fmt.Println(foo(mySlice...))
+
+	fmt.Println(bar(mySlice))
 }
 func foo(a ...int) int {
 	sum := 0
@@ -16,3 +18,22 @@ func foo(a ...int) int {
 	}
 	return sum
 }
+func bar(a []int) int {
+	sum := 0
+	for _, v := range a {
+		sum = sum + v
+	}
+	return sum
+}
+
+/*
+create a func with the identifier foo that
+takes in a variadic parameter of type int
+pass in a value of type []int into your func (unfurl the []int)
+returns the sum of all values of type int passed in
+
+create a func with the identifier bar that
+takes in a parameter of type []int
+returns the sum of all values of type int passed in
+
+*/
